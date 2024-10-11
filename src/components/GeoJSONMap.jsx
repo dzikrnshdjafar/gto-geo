8aca62cd256583d31bebd13d8eccd7a7d5f7c93b
bukanprogrammr <dzikriansah_s1sisfo2019@mahasiswa.ung.ac.id>
2024-10-12 00:57:07 +0700

    add loadingtodd

--- src/components/GeoJSONMap.jsx
+++ src/components/GeoJSONMap.jsx
@@ -4,6 +4,7 @@ import axios from 'axios';
 
 const GeoJSONMap = () => {
   const [geoData, setGeoData] = useState([]);
+  const [loading, setLoading] = useState(true);
 
   // Array color GeoJSON
   const earthPalette = [
@@ -18,13 +19,19 @@ const GeoJSONMap = () => {
     "#687864", // Sagebrush Green
     "#BDA27F"  // Desert Beige
   ];  
-  
 
   useEffect(() => {
     // Fetch all GeoJSON
+    setLoading(true);
     axios.get(`${import.meta.env.VITE_APP_API_GEO}`)
-      .then(response => setGeoData(response.data))
-      .catch(error => console.error('Error fetching geojson:', error));
+      .then(response => {
+        setGeoData(response.data);
+        setLoading(false);
+      })
+      .catch(error => {
+        console.error('Error fetching geojson:', error);
+        setLoading(false);
+      });
   }, []);
 
   // Function for geoJSON color
@@ -45,43 +52,49 @@ const GeoJSONMap = () => {
   };
 
   return (
-    <MapContainer center={[0.7047498986879579, 122.43409669232882]} zoom={9} style={{ height: '500px', width: '100%' }}>
-      <TileLayer
-      url="https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png"
-      attribution="Tiles © Esri — Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community"
-    />
-      <LayersControl position="topright">
-        {geoData.map((geojson, index) => (
-          <LayersControl.Overlay 
-            key={index} 
-            name={geojson.name} 
-            checked 
-          >
-            <GeoJSON
-              data={geojson}
-              style={() => getGeoJSONStyle(index)}
-              onEachFeature={(feature, layer) => {
-                if (feature.properties && feature.properties.NAMOBJ) {
-                  layer.bindPopup(`<div><strong>${feature.properties.NAMOBJ}</strong></div>`);
-                }
+    <>
+      {loading ? (
+        <div style={{ textAlign: 'center', padding: '20px' }}>Memuat data...</div>
+      ) : (
+        <MapContainer center={[0.7047498986879579, 122.43409669232882]} zoom={9} style={{ height: '500px', width: '100%' }}>
+          <TileLayer
+            url="https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png"
+            attribution="Tiles © Esri — Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community"
+          />
+          <LayersControl position="topright">
+            {geoData.map((geojson, index) => (
+              <LayersControl.Overlay 
+                key={index} 
+                name={geojson.name} 
+                checked 
+              >
+                <GeoJSON
+                  data={geojson}
+                  style={() => getGeoJSONStyle(index)}
+                  onEachFeature={(feature, layer) => {
+                    if (feature.properties && feature.properties.NAMOBJ) {
+                      layer.bindPopup(`<div><strong>${feature.properties.NAMOBJ}</strong></div>`);
+                    }
 
-                // styling geoJSON hover
-                layer.on({
-                  mouseover: (e) => {
-                    const targetLayer = e.target;
-                    targetLayer.setStyle(getGeoJSONHover(index));
-                  },
-                  mouseout: (e) => {
-                    const targetLayer = e.target;
-                    targetLayer.setStyle(getGeoJSONStyle(index));
-                  }
-                });
-              }}
-            />
-          </LayersControl.Overlay>
-        ))}
-      </LayersControl>
-    </MapContainer>
+                    // styling geoJSON hover
+                    layer.on({
+                      mouseover: (e) => {
+                        const targetLayer = e.target;
+                        targetLayer.setStyle(getGeoJSONHover(index));
+                      },
+                      mouseout: (e) => {
+                        const targetLayer = e.target;
+                        targetLayer.setStyle(getGeoJSONStyle(index));
+                      }
+                    });
+                  }}
+                />
+              </LayersControl.Overlay>
+            ))}
+          </LayersControl>
+        </MapContainer>
+      )}
+    </>
   );
 };
 
